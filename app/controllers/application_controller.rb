f395f7084dc710db647c255e364c505cde80c7f2
Dani DeSalvio <danidvisuals@gmail.com>
2019-12-02 20:34:31 -0700

    done with devise for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+  # def user_params
+  #   params.require(:user)(:sign_up).permit(:email, :password, :first_name, :last_name)
+  # end
+  # the code above explains what the code below is DOING
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 end
